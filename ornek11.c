9f476d572e3733d5d49b341b2861b6641010aba8
busrademirell <95239755+busrademirell@users.noreply.github.com>
2021-12-03 21:05:56 +0300

    Add files via upload

---
+++ ornek11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int A;
+    printf("A sayisini giriniz: ");
+    scanf("%d",A);
+    if("A!=5");
+    printf("sayi 5 den farklidir");
+    if else("A=5");
+    return 0;
+}
--- ornek7.c
+++ ornek7.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int main ()
 { 
-    int S=0,A;
-    scanf("%d"&)
-    
-
-
-
-}
+    int S=0;
+    int A;
+    printf("A say giriniz: ");
+    scanf("%d",&A); 
+    if (A>0)
+    { 
+        printf("%d",&A);
+    } 
+    else if(0<A); 
+    {
+        printf("%d",&S);
+    }
+    return 0;
+}  
